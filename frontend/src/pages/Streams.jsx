753409f57012118bff618a752be96723b003feae
jjdigby85 <279348937+jjdigby85@users.noreply.github.com>
2026-07-29 16:22:19 -0400

    Update Streams.jsx

--- frontend/src/pages/Streams.jsx
+++ frontend/src/pages/Streams.jsx
@@ -47,9 +47,9 @@ const PageContent = () => {
   }
 
   return (
-     <Box h={'100vh'} w={'100%'} display={'flex'} style={{ overflowX: 'auto' }}>
-        <Box p={10} style={{ overflowX: 'auto' }}>
-          <Box w={'100%'}>
+    <Box h={'100vh'} w={'100%'} display={'flex'} style={{ overflowX: 'auto' }}>
+        <Box p={10} miw={'100px'} style={{ overflowX: 'auto' }}>
+          <Box miw={'100vh'}>
             <StreamsTable onReady={handleStreamsReady} />
           </Box>
         </Box>
