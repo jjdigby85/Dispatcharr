1106b8f6ecf176c7c47ae4830438d23359509fe0
jjdigby85 <279348937+jjdigby85@users.noreply.github.com>
2026-07-30 16:54:00 -0400

    Update StreamsTable.jsx

--- frontend/src/components/tables/StreamsTable.jsx
+++ frontend/src/components/tables/StreamsTable.jsx
@@ -21,6 +21,8 @@ import {
   SquareCheck,
   SquareMinus,
   SquarePlus,
+  Play,
+  link,
 } from 'lucide-react';
 import {
   ActionIcon,
@@ -131,6 +133,7 @@ const StreamRowActions = ({
 
   return (
     <>
+      {/*
       <Tooltip label="Add to Channel" openDelay={500}>
         <ActionIcon
           size={iconSize}
@@ -149,7 +152,21 @@ const StreamRowActions = ({
           <ListPlus size="18" fontSize="small" />
         </ActionIcon>
       </Tooltip>
+     */}
 
+      <Tooltip label="Preview Stream" openDelay={500}>
+        <ActionIcon
+          size={iconSize}
+          color={theme.tailwind.green[5]}
+          variant="transparent"
+          onClick={onPreview}
+          style={{ background: 'none' }}
+        >
+          <Play size="18" fontSize="small" />
+        </ActionIcon>
+      </Tooltip>
+
+      {/*
       <Tooltip label="Create New Channel" openDelay={500}>
         <ActionIcon
           size={iconSize}
@@ -157,10 +174,24 @@ const StreamRowActions = ({
           variant="transparent"
           onClick={() => handleCreateChannelFromStream(row.original)}
         >
-          <SquarePlus size="18" fontSize="small" />
+          <Play size="18" fontSize="small" />
+        </ActionIcon>
+      </Tooltip>
+      */}
+
+      <Tooltip label="Copy URL" openDelay={500}>
+        <ActionIcon
+          size={iconSize}
+          color={theme.tailwind.blue[6]}
+          variant="transparent"
+          onClick={() => copyToClipboard(row.original.url)}
+          style={{ background: 'none' }}
+        >
+          <link size="18" fontSize="small" />
         </ActionIcon>
       </Tooltip>
 
+      {/*
       <Menu>
         <MenuTarget>
           <ActionIcon variant="transparent" size={iconSize}>
@@ -189,6 +220,7 @@ const StreamRowActions = ({
           </MenuItem>
         </MenuDropdown>
       </Menu>
+      */}
     </>
   );
 };
@@ -259,7 +291,7 @@ const StreamsTable = ({ onReady }) => {
   // Default hidden: tvg_id, stats
   const DEFAULT_COLUMN_VISIBILITY = {
     actions: true,
-    select: true,
+    select: false,
     name: true,
     group: true,
     m3u: true,
@@ -1387,6 +1419,7 @@ const StreamsTable = ({ onReady }) => {
           style={{ padding: 10 }}
           gap={6}
         >
+          {/*
           <Flex gap={6} wrap="nowrap" style={{ flexShrink: 0 }}>
             <Tooltip
               label="Add selected stream(s) to the target channel"
@@ -1440,7 +1473,7 @@ const StreamsTable = ({ onReady }) => {
               </Button>
             </Tooltip>
           </Flex>
-
+          */}
           <Flex gap={6} wrap="nowrap" style={{ flexShrink: 0 }}>
             <Menu shadow="md" width={200}>
               <MenuTarget>
@@ -1490,7 +1523,7 @@ const StreamsTable = ({ onReady }) => {
                 </MenuItem>
               </MenuDropdown>
             </Menu>
-
+            {/*
             <Tooltip label="Create a new custom stream" openDelay={500}>
               <Button
                 leftSection={<SquarePlus size={18} />}
@@ -1520,7 +1553,7 @@ const StreamsTable = ({ onReady }) => {
                 Delete
               </Button>
             </Tooltip>
-
+            */}
             <Menu shadow="md" width={200}>
               <MenuTarget>
                 <Tooltip label="Table Settings" openDelay={500}>
